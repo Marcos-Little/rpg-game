4034b5651510d0294fb283febe97de44dc345282
Marcos Oliveira <aoliveirajunio0@gmail.com>
2025-07-30 11:01:51 -0300

    Update personagens.js

--- js/personagens.js
+++ js/personagens.js
@@ -1,5 +1,5 @@
 let players = [document.getElementById("jogador-1"),document.getElementById("jogador-2"), document.getElementById("jogador-3"), document.getElementById("jogador-4"), document.getElementById("jogador-5"), document.getElementById("jogador-6")]
-let contador = 0
+let contador = 1
 
 function adicionar() {
     if(contador <= 5{
@@ -9,19 +9,19 @@ function adicionar() {
     }
 }    
 function remover(){
-    if(contador <=4){
+    if(contador <=5 ){
     players[contador].classList.remove("selecionado");
     players[contador].classList.add("selecao");
     contador--}
     else if(contador == 5){
-            players[contador].classList.remove("selecionado");
+        players[contador].classList.remove("selecionado");
         players[contador].classList.add("selecao");
         contador--
     }
-    if(contador < 0){
-        contador = 0;
+    if(contador < 1){
+        contador = 1;
     }
-    if(contador == 0){
+    if(contador == 1){
         players[contador].classList.add("selecionado");
     }
 }
